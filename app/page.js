50b0d56956cbde0d583d413cc8b878edc082363e
rafatparween <rafatparween27392@gmail.com>
2025-08-25 13:38:23 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+
+import Image from "next/image";
+import Contents from "./Components/Contents";
+import Navbar from "./Components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Contents/>
+
+    </>
+  );
+}
